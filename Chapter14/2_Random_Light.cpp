7778ead59021f6c9c2af0742b6f8d5079ef25c66
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2024-05-02 10:01:34 +0900

    무작위로 켜기

--- Chapter14/2_Random_Light.cpp
+++ Chapter14/2_Random_Light.cpp
@@ -6,8 +6,7 @@ BusOut leds(D4, D5, D6, D7, D8, D9, D10, D11, D12);
 
 int main() {
     leds = 0x1FF;       // access all of the leds to turn them off
-    while(true)
-    {
+    while(true) {
         leds = rand();  // assign random number to leds
         ThisThread::sleep_for(1s);
     }
