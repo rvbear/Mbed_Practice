dd18bdee8dbdf1bcb5282ab17b9e31540f663beb
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2024-05-02 11:00:42 +0900

    FND에 순차 표시

---
+++ Chapter14/4_7-Segments.cpp
@@ -0,0 +1,14 @@
+#include "mbed.h"
+
+BusOut fnd(D2, D3, D4, D5, D6, D7, D8, D9);
+uint16_t num[] = {0x77, 0x41, 0x3B, 0x6B, 0x4D, 0x6E, 0x7E, 0x43, 0x7F,
+                    0x4F, 0x7B, 0x7C, 0x38, 0x79, 0x3E, 0x1E};      //FND DATA
+
+int main() {
+    fnd = 0xFF;     // all off
+    uint16_t cnt = 0;
+    while(true) {
+        fnd = num[cnt++ % 16];
+        ThisThread::sleep_for(500ms);
+    }
+}
